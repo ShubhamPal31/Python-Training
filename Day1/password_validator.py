49daf1c4684f3d76c325a97695b5dde72d555073
shubhampal31 <shubhampal31102003@gmail.com>
2025-12-29 18:07:35 +0530

    code added for password validator

---
+++ Day1/password_validator.py
@@ -0,0 +1,37 @@
+def main():
+    password = input("Enter your password: ")
+    is_valid = False
+    for i in range(2):
+        if(len(password)<6):
+            print("Password too short!!. It  should be atleast 6 characters long.")
+            password = input("Please try again: ")
+        elif not(has_numeric_character(password)):
+            print("Invalid Password!!. It  should atleast have one numeric character.")
+            password = input("Please try again: ")
+        elif not(has_uppercase_character(password)):
+            print("Invalid Password!!. It  should atleast have one uppercase character.")
+            password = input("Please try again: ")
+        else:
+            is_valid = True
+            break
+    
+    if(is_valid):
+        print("Valid Password, it matches all the requirements")
+    else:
+        print("You have exhausted all your attempts!!")
+
+
+def has_numeric_character(str):
+    for ch in str:
+        if (ch>='0' and ch<='9'):
+            return True
+    return False
+
+def has_uppercase_character(str):
+    for ch in str:
+        if (ch>='A' and ch<='Z'):
+            return True
+    return False
+
+if __name__=="__main__":
+    main()
