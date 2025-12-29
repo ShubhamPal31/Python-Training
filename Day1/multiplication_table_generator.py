0ee6227abd6f9af918cd3756e9549e066bbd7609
shubhampal31 <shubhampal31102003@gmail.com>
2025-12-29 18:19:09 +0530

    Modified multiplication table generator code

--- Day1/multiplication_table_generator.py
+++ Day1/multiplication_table_generator.py
@@ -1,7 +1,10 @@
 def main():
     num = int(input("Enter a number: "))
     end = int(input("Enter the last number you want in the multiplication table: "))
-    print(f"The multiplication table of {num} is:")
+    if end<1:
+        print("Invalid range")
+        return
+    print(f"The multiplication table of {num} from 1 to {end} is:")
     for i in range(1, end+1):
         print(f"{num} * {i} = {num*i}")
 
