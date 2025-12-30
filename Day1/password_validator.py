f83e1bacca433251c10cfc94f3855b8a94ee7168
shubhampal31 <shubhampal31102003@gmail.com>
2025-12-30 11:26:25 +0530

    Day 1 Assginment added

--- Day1/password_validator.py
+++ Day1/password_validator.py
@@ -2,14 +2,20 @@ def main():
     password = input("Enter your password: ")
     is_valid = False
     for i in range(2):
-        if(len(password)<6):
-            print("Password too short!!. It  should be atleast 6 characters long.")
+        if(len(password)<8):
+            print("Password too short!!. It  should be atleast 8 characters long.")
             password = input("Please try again: ")
         elif not(has_numeric_character(password)):
             print("Invalid Password!!. It  should atleast have one numeric character.")
             password = input("Please try again: ")
         elif not(has_uppercase_character(password)):
             print("Invalid Password!!. It  should atleast have one uppercase character.")
+            password = input("Please try again: ")
+        elif not(has_lowercase_character(password)):
+            print("Invalid Password!!. It  should atleast have one lowercase character.")
+            password = input("Please try again: ")
+        elif not(has_special_character(password)):
+            print("Invalid Password!!. It  should atleast have one special character.")
             password = input("Please try again: ")
         else:
             is_valid = True
@@ -30,6 +36,19 @@ def has_numeric_character(str):
 def has_uppercase_character(str):
     for ch in str:
         if (ch>='A' and ch<='Z'):
+            return True
+    return False
+
+def has_lowercase_character(str):
+    for ch in str:
+        if (ch>='a' and ch<='z'):
+            return True
+    return False
+
+def has_special_character(str):
+    allowed_specials = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~"
+    for ch in str:
+        if (ch in allowed_specials):
             return True
     return False
 
